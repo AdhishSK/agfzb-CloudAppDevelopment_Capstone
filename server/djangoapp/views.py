a19bad70192d10bb1da3aecac633f377e6eb5627
Adhish <adhish@gmail.com>
2023-10-17 01:00:34 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -69,7 +69,7 @@ def login_request(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            #messages.success(request, "Login successfully!")
+            messages.success(request, "Login successfully!")
             return redirect('djangoapp:index')
         else:
             messages.warning(request, "Invalid username or password.")
